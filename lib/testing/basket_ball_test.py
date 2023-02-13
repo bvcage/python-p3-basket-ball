c25980e0b7a5b6f5f3943eb305dea1a2b0e176a2
Bailey Cage <bailey.cage@gmail.com>
2023-02-13 13:32:09 -0500

    update test_average_rebouts_by_shoe_brand() to check for 1 space between brand & average, to match README instructions

--- lib/testing/basket_ball_test.py
+++ lib/testing/basket_ball_test.py
@@ -90,4 +90,4 @@ class TestBasketBall:
       sys.stdout = captured_out
       average_rebounds_by_shoe_brand()
       sys.stdout = sys.__stdout__
-      assert(captured_out.getvalue() == "Nike:  4.93\nAdidas:  7.07\nPuma:  8.50\nJordan:  3.80\n")
+      assert(captured_out.getvalue() == "Nike: 4.93\nAdidas: 7.07\nPuma: 8.50\nJordan: 3.80\n")
